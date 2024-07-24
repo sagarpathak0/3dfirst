a2add1bc10e06ed0383930cec8eca012303cfa74
sagarpathak0 <sagar200422@gmail.com>
2024-07-25 01:26:32 +0530

    secondd commiet

--- pages/layout.tsx
+++ pages/layout.tsx
@@ -1,14 +1,19 @@
 import Navbar from "./navbar";
 import Footer from "./footer";
 import Scene from "@/components/Scene";
-import 'tailwindcss/tailwind.css'
+import 'tailwindcss/tailwind.css';
+import { ReactNode } from 'react';
 
-export default function Layout({children}){
-    return(
-        <>
-            <Navbar/>
-            <main>{children}</main>
-            <Footer/>
-        </>
-    )
+interface LayoutProps {
+  children: ReactNode;
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Navbar />
+      <main>{children}</main>
+      <Footer />
+    </>
+  );
 }
